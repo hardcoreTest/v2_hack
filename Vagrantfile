81aca5e8751cc28ef9c77642552abd8c90097158
hardcore_test <mail@vladbykov.ru>
2019-05-18 16:37:21 +0500

    update vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -2,8 +2,8 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
-  config.vm.box = "ubuntu/trusty64-daily-1"
-  config.vm.box_url = "https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
+  config.vm.box = "ubuntu/xenial64-daily"
+  config.vm.box_url = "https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-vagrant.box"
 
 
   config.vm.network "forwarded_port", guest: 80, host: 8085
@@ -13,7 +13,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.provider :virtualbox do |vb|
       vb.customize ["modifyvm", :id, "--ostype", "Ubuntu_64"]
       vb.customize ["modifyvm", :id, "--memory", 2568]
-      vb.customize ["modifyvm", :id, "--cpus", "2"]
+      vb.customize ["modifyvm", :id, "--cpus", "3"]
       vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
       vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
   end
--- vagrant_provision/bootstrap.sh
+++ vagrant_provision/bootstrap.sh
@@ -1,18 +1,35 @@
 if [ -f "/.bootstrap.done" ]; then
     service apache2 restart
+    service elasticsearch restart
     exit
 fi
 
+DEBIAN_FRONTEND=noninteractive apt-get -y update
+DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
+DEBIAN_FRONTEND=noninteractive apt-get -y install software-properties-common python-software-properties
 DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:ondrej/php
 DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:ondrej/apache2
 DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:webupd8team/java
 DEBIAN_FRONTEND=noninteractive apt-get -y update
+DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 DEBIAN_FRONTEND=noninteractive apt-get -y install php7.0
 DEBIAN_FRONTEND=noninteractive apt-get -y install apache2
 DEBIAN_FRONTEND=noninteractive apt-get -y install mysql-server
 DEBIAN_FRONTEND=noninteractive apt-get -y install libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json php7.0-dom\
- php7.0-gd php7.0-zip
+ php7.0-gd php7.0-zip php7.0-dev unzip php7.0-mbstring php-xdebug memcached php7.0-memcached
 
+echo "
+zend_extension=xdebug.so
+xdebug.remote_enable=1
+xdebug.remote_handler=dbgp
+xdebug.remote_host=127.0.0.1
+xdebug.remote_port=9001
+xdebug.remote_autostart=0
+xdebug.remote_connect_back=0" > /etc/php/7.0/mods-available/xdebug.ini
+
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+DEBIAN_FRONTEND=noninteractive apt-get -y install nodejs
 
 rm /etc/apache2/sites-enabled/000-default.conf
 ln -s /vagrant/vagrant_provision/app.conf /etc/apache2/sites-enabled/001-app.conf
@@ -20,10 +37,17 @@ ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/rewrite
 service apache2 restart
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '93b54496392c062774670ac18b134c3b3a95e5a5e5c8f1a9f115f203b75bf9a129d5daa8ba6a13e2cc8a1da0806388a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-
 php composer-setup.php --install-dir=/bin --filename=composer
 php -r "unlink('composer-setup.php');"
 
+
+
+adduser --quiet --home /home/vagrant vagrant
+usermod -a -G sudo vagrant
+echo "vagrant:vagrant" | chpasswd
+
+cat /etc/ssh/sshd_config | sed -e 's/PasswordAuthentication no/#PasswordAuthentication no/g' > /root/22.txt
+mv /root/22.txt /etc/ssh/sshd_config
+service sshd restart
 
 touch /.bootstrap.done
