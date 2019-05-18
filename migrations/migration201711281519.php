f7af1239a7e02d9347d612a0e7060971ba2e37b1
hardcore_test <mail@vladbykov.ru>
2019-05-18 16:54:41 +0500

    add model and migration

---
+++ migrations/migration201711281519.php
@@ -0,0 +1,33 @@
+<?php
+
+use Doctrine\DBAL\Schema\Table;
+use \Doctrine\DBAL\Types\Type;
+
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 21.02.2017
+ * Time: 16:21
+ */
+class migration201711281519
+{
+    public function up(Doctrine\DBAL\Schema\AbstractSchemaManager $schema, Doctrine\DBAL\Connection $connection)
+    {
+        if (!$schema->tablesExist('user')) {
+            $users = new Table('user');
+            $users->addColumn('id', Type::GUID, ['notnull' => false]);
+            $users->setPrimaryKey(['id']);
+            $users->addColumn('type_user', Type::GUID, ['length' => 64]);
+            $users->addIndex(['type_user'], 'type_user');
+            $users->addColumn('type_waste', Type::BLOB);
+            $users->addIndex(['type_waste'], 'type_waste');
+            $users->addColumn('coordinate', Type::BLOB);
+            $users->addColumn('contact', Type::STRING, ['length' => 255, 'notnull' => false]);
+            $users->addColumn('email', Type::STRING, ['length' => 255]);
+            $users->addColumn('date_create', Type::DATETIME );
+            $users->addColumn('date_update', Type::DATETIME );
+            $schema->createTable($users);
+
+        }
+    }
+}
--- src/backend/Model/Schedule.php
+++ src/backend/Model/Schedule.php
@@ -12,75 +12,189 @@ namespace schedule\Model;
 class Schedule extends AbstractDbModel
 {
     const FIELD_ID = "id";
-    const FIELD_DATE = "date";
-    const FIELD_LESSON = "lesson";
+    const FIELD_NAME = "name";
+    // user, collector, utilizer
+    const FIELD_TYPE_USER = "type_user";
+//    paper, хуибала, дрыгалы
+    const FIELD_TYPE_WASTE = "type_waste";
+    const FIELD_COORDINATE = "coordinate";
+    const FIELD_CONTACT = "contact";
+    const FIELD_EMAIL = "email";
+    const FIELD_DATE_CREATE = "date_create";
+    const FIELD_DATE_UPDATE = "date_update";
 
-    const TABLE_NAME = "schedule";
+    const TABLE_NAME = "users";
     /**
      * @var string $id ;
      */
     protected $id;
 
-    /**
-     * @var string $date ;
-     */
-    protected $date;
-    /**
-     * @var string $lesson ;
-     */
-    protected $lesson;
+    /** @var string */
+    protected $name;
+    /** @var string */
+    protected $type_user;
+    /** @var string */
+    protected $type_waste;
+    /** @var string */
+    protected $coordinate;
+/** string */
+    protected $contact;
+    /** @var string */
+    protected $date_create;
+    /** @var string */
+    protected $date_update;
 
     protected $tableName = self::TABLE_NAME;
     protected $keyField = 'id';
     protected $fieldMap = array(
         'id' => 'id',
-        'date' => 'date',
-        'lesson' => 'lesson',
+        'name' => 'name',
+        'type_user' => 'type_user',
+        'type_waste' => 'type_waste',
+        'coordinate' => 'coordinate',
+        'contact' => 'contact',
+        'date_create' => 'date_create',
+        'date_update' => 'date_update',
     );
-
 
     /**
      * @return string
      */
-    public function getId()
+    public function getId(): string
     {
         return $this->id;
     }
 
-
     /**
-     * @return string
+     * @param string $id
      */
-    public function getDate()
+    public function setId(string $id)
     {
-        return $this->date;
-    }
-
-    /**
-     * @param string $date
-     */
-    public function setDate($date)
-    {
-        $this->date = $date;
-        $this->dirtyField('date');
+        $this->id = $id;
+        $this->dirtyField('id');
     }
 
     /**
      * @return string
      */
-    public function getLesson()
+    public function getName(): string
     {
-        return $this->lesson;
+        return $this->name;
     }
 
     /**
-     * @param string $lesson
+     * @param string $name
      */
-    public function setLesson($lesson)
+    public function setName(string $name)
     {
-        $this->lesson = $lesson;
-        $this->dirtyField('lesson');
+        $this->name = $name;
+        $this->dirtyField('name');
     }
+
+    /**
+     * @return string
+     */
+    public function getTypeUser(): string
+    {
+        return $this->type_user;
+    }
+
+    /**
+     * @param string $type_user
+     */
+    public function setTypeUser(string $type_user)
+    {
+        $this->type_user = $type_user;
+        $this->dirtyField('type_user');
+    }
+
+    /**
+     * @return string
+     */
+    public function getTypeWaste(): string
+    {
+        return $this->type_waste;
+    }
+
+    /**
+     * @param string $type_waste
+     */
+    public function setTypeWaste(string $type_waste)
+    {
+        $this->type_waste = $type_waste;
+        $this->dirtyField('type_waste');
+    }
+
+    /**
+     * @return string
+     */
+    public function getCoordinate(): string
+    {
+        return $this->coordinate;
+    }
+
+    /**
+     * @param string $coordinate
+     */
+    public function setCoordinate(string $coordinate)
+    {
+        $this->coordinate = $coordinate;
+        $this->dirtyField('coordinate');
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContact()
+    {
+        return $this->contact;
+    }
+
+    /**
+     * @param mixed $contact
+     */
+    public function setContact($contact)
+    {
+        $this->contact = $contact;
+        $this->dirtyField('contact');
+    }
+
+    /**
+     * @return string
+     */
+    public function getDateCreate(): string
+    {
+        return $this->date_create;
+    }
+
+    /**
+     * @param string $date_create
+     */
+    public function setDateCreate(string $date_create)
+    {
+        $this->date_create = $date_create;
+        $this->dirtyField('date_create');
+    }
+
+    /**
+     * @return string
+     */
+    public function getDateUpdate(): string
+    {
+        return $this->date_update;
+    }
+
+    /**
+     * @param string $date_update
+     */
+    public function setDateUpdate(string $date_update)
+    {
+        $this->date_update = $date_update;
+        $this->dirtyField('date_update');
+    }
+
+
+
 
 
 
